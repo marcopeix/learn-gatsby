ea149484797ae02298b99f5867027c1f2329a5c4
Marco Peixeiro <marcopeix@outlook.com>
2018-05-29 22:28:25 -0400

    Added layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,5 +39,13 @@ export default () =>
       </div>
       <p>Posted April 09, 2011</p>
     </div>
-    
+
+    <div>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+    </div>
+
   </div>
